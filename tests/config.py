4c2a4d803e5fe3712a887c2b256dde8e4e48d12d
ayananerv <ayanaminerv@hotmail.com>
2026-01-19 17:04:50 +0800

    build basic structure and initial test

---
+++ tests/config.py
@@ -0,0 +1,10 @@
+# test_buffered_writer
+from pathlib import Path
+
+_PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+OUTPUT_DIR = _PROJECT_ROOT / "temp" / "output"
+
+STR_LEN = 16
+
+WORKER_ROUND = 500
